6bb3ea1d81d98445cd6f9f1ed026f73fd24b8a37
Anthony Ledru <ledru.anthony51@gmail.com>
2018-03-09 16:26:05 +0100

    Update ft_strjoin.c

--- srcs/str/ft_strjoin.c
+++ srcs/str/ft_strjoin.c
@@ -19,24 +19,18 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		i;
 
 	i = 0;
-	size = 0;
-	if (s1 == NULL || s2 == NULL)
+	size = ft_strlen(s1) + ft_strlen(s2);
+	if (!(res = ft_memalloc(sizeof(char) * size + 1)))
 		return (NULL);
-	size = ft_strlen(s1);
-	size += ft_strlen(s2);
-	if (NULL != (res = ft_strnew(size)))
+	while (*s1)
 	{
-		while (*s1)
-		{
-			res[i++] = *s1;
-			s1++;
-		}
-		while (*s2)
-		{
-			res[i++] = *s2;
-			s2++;
-		}
-		return (res);
+		res[i++] = *s1;
+		s1++;
 	}
-	return (NULL);
+	while (*s2)
+	{
+		res[i++] = *s2;
+		s2++;
+	}
+	return (res);
 }
