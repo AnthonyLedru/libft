ca84c3e9300d860c329ace3db5465169c72546a1
Anthony LEDRU <aledru@e1r2p15.42.fr>
2018-03-10 13:25:10 +0100

    fix strjoin

--- srcs/str/ft_strjoin.c
+++ srcs/str/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: aledru <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 02:32:52 by aledru            #+#    #+#             */
-/*   Updated: 2017/11/09 02:47:46 by aledru           ###   ########.fr       */
+/*   Updated: 2018/03/10 13:22:33 by aledru           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		i;
 
 	i = 0;
+	if (!s1 || !s2)
+		return (NULL);
 	size = ft_strlen(s1) + ft_strlen(s2);
 	if (!(res = ft_memalloc(sizeof(char) * size + 1)))
 		return (NULL);
